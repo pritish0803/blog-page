a8d6f22ebaf9b902708ee913befaa5f1055730df
pritish gupta <pritish9040246119@gmail.com>
2017-10-12 20:23:43 +0530

    Create models.py

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+
+from django.db import models
+
+# Create your models 
+class Post(models.Model):
+	title=models.CharField(max_length=100)
+	body=models.TextField(null=True)
+	date=models.DateTimeField()
+	job=models.TextField(null=True)
+	
+	def __unicode__(self):
+		return self.title
